bf4febbffb4ff89c8f9e3bf659c671d1552811b8
Justin Chang <33967054+Juniorn1003@users.noreply.github.com>
2017-12-23 05:49:24 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system(" sudo apt-get install python-pip python-dev build-essential -y")
-os.system("sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-setuptools -y")
-os.system("sudo apt-get install python-selenium && sudo apt-get install firefoxdriver -y")
-os.system("pip install mechanize && pip install paramiko && pip install requests && pip install xmpppy==0.5.0rc1")
-os.system("pip install stem && pip install pyvirtualdisplay && apt-get install xvfb")
