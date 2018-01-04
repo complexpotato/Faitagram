3783f25dfeec8fc29bc95f6a5a60ed8cf145c03e
Justin Chang <33967054+Juniorn1003@users.noreply.github.com>
2018-01-04 10:54:39 -0500

    torrc modify & gekodriver compatible version fix
    This script modifies torrc, to remove the [Errno111] issue, and I changed the link of geckodriver because version 0.9.0 is buggy.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.system("sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 os.system("pip install -U selenium && sudo apt-get install firefoxdriver -y")
 os.system("pip install mechanize && pip install paramiko && pip install requests && pip install xmpppy==0.5.0rc1")
 os.system("pip install stem && pip install pyvirtualdisplay && apt-get install xvfb")
-os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.9.0/geckodriver-v0.9.0-linux64.tar.gz")
+os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.13.0/geckodriver-v0.13.0-linux64.tar.gz")
 os.system("tar -xvzf geckodriver-v0.9.0-linux64.tar.gz")
 os.system("rm geckodriver-v0.9.0-linux64.tar.gz")
 os.system("chmod +x geckodriver")
