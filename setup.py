140c16a9ac14738e3d581852256ad35e9c2fea30
Justin Chang <33967054+Juniorn1003@users.noreply.github.com>
2018-01-18 06:33:30 -0500

    OS Bit Calculating system added

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
-import os
+import os, math, sys
+
+OS_bit = (round(math.log(sys.maxint,2)+1))
 
 os.system("sudo apt-get install python-pip python-dev build-essential -y")
 os.system("sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-setuptools -y")
 os.system("pip install -U selenium && sudo apt-get install firefoxdriver -y")
 os.system("pip install mechanize &&  pip install requests && pip install xmpppy==0.5.0rc1")
 os.system("pip install stem && pip install pyvirtualdisplay && apt-get install xvfb")
-os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz")
-os.system("tar -xvzf geckodriver-v0.19.0-linux64.tar.gz")
-os.system("rm geckodriver-v0.19.0-linux64.tar.gz")
+
+print OS_bit
+
+if OS_bit == 64:
+
+    os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz")
+    os.system("tar -xvzf geckodriver-v0.19.0-linux64.tar.gz")
+    os.system("rm geckodriver-v0.19.0-linux64.tar.gz")
+
+elif OS_bit == 32:
+
+    os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux32.tar.gz")
+    os.system("tar -xvzf geckodriver-v0.19.0-linux32.tar.gz")
+    os.system("rm geckodriver-v0.19.0-linux32.tar.gz")
+
 os.system("chmod +x geckodriver")
 os.system("mv geckodriver /usr/local/bin/")
 
