2802b062ae2c0172ad092b22354adaa4a9f94276
Justin Chang <33967054+Juniorn1003@users.noreply.github.com>
2018-02-22 09:14:48 -0500

    Do not need torrc anymore

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ print("\n \n {} \n \n".format(OS_bit))
 
 
 os.system('firefox -v > tmp')                  # store result of firefox -v in tmp
-result       =  open('tmp', 'r').read()        # result var reads the output
-marker       = result.find('Firefox') + 8      # marker marks the 8th letter from the word "Firefox"
-version      = result[marker:].splitlines()[0] # spliting the output, the version is something like aa.bb.cc
-a,b,c = version.split(".")              # firefoxV is the var with the aa
+result   =  open('tmp', 'r').read()            # result var reads the output
+marker   = result.find('Firefox') + 8          # marker marks the 8th letter from the word "Firefox"
+version  = result[marker:].splitlines()[0]     # spliting the output, the version is something like aa.bb.cc
+a,b,c = version.split(".")                     # a is the var with the aa
 os.remove('tmp')                               # removing the temporary file
 
 
@@ -49,18 +49,3 @@ os.system("tar -xvzf geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,bi
 os.system("rm geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,bit))
 os.system("chmod +x geckodriver")
 os.system("mv geckodriver /usr/local/bin/")
-
-fp = open("/etc/tor/torrc")
-fx = open("/etc/tor/torrc2","w+")     #torrc with edits
-
-for i, line in enumerate(fp):         #some reading and editing action going on here
-    if i+1 == 57 or i+1 == 61:
-        line = line.replace("#","")
-    fx.write(line)
-fp.close()
-fx.close()
-
-os.system("rm /etc/tor/torrc && mv /etc/tor/torrc2 /etc/tor/torrc")
-os.system("service tor restart && service tor stop")
-
-print("\ntorrc modification success\n")
