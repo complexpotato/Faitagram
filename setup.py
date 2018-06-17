5025bc0245ba07c95bfd665dc0ca5b00eee60255
josiahgithub <33040505+josiahgithub@users.noreply.github.com>
2018-06-17 18:51:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os, math, sys
 
-OS_bit = (round(math.log(sys.maxint,2)+1))  # get the bit
+#OS_bit = (round(math.log(sys.maxint,2)+1))  # get the bit
 
 os.system("sudo apt-get install python-pip && sudo apt-get install tor")   # installing dependencies
 os.system("pip install -U selenium")
 os.system("pip install Pysocks")
 os.system("pip install pyvirtualdisplay && apt-get install xvfb")
 
-print("\n \n {} \n \n".format(OS_bit))
+#print("\n \n {} \n \n".format(OS_bit))
 
 
 os.system('firefox -v > tmp')                  # store result of firefox -v in tmp
